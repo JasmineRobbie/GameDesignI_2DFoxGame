e9544dd98541bf611f4e8f63d57abd93fe678a3a
Jay <jrabie@umich.edu>
2021-10-13 12:45:53 -0400

    Initial Commit

---
+++ New Unity Project/Assets/MoveCharacter.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCharacter : MonoBehaviour
+{
+
+    public float speed = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        
+        //if(Input.GetKey("w"))
+        //{
+        //    pos.y += speed * Time.deltaTime;
+        //}
+        //if (Input.GetKey("s"))
+        //{
+        //    pos.y -= speed * Time.deltaTime;
+        //} 
+
+        if (Input.GetKey("d"))
+        {
+            pos.x += speed * Time.deltaTime;
+        }
+        if (Input.GetKey("a"))
+        {
+            pos.x -= speed * Time.deltaTime;
+        }
+        if (Input.GetKey("space"))
+        {
+            pos.y += speed * Time.deltaTime;
+        }
+
+        transform.position = pos;
+    }
+}
