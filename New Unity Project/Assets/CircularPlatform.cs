a077db6fcacd6b89c03624fc502a4a0af57dd726
cjlovelace <50459158+cjlovelace@users.noreply.github.com>
2021-11-01 21:57:33 -0400

    Updated camera. Bug: Fox sprite disappears.

--- New Unity Project/Assets/CircularPlatform.cs
+++ New Unity Project/Assets/CircularPlatform.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Feet : MonoBehaviour
+public class CircularPlatform : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
