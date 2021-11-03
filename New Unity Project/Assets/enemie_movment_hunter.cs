117c3d62d252bb9a93b27d43540a7f74b3c4cfdd
Cam <58830545+camie730@users.noreply.github.com>
2021-11-03 01:14:13 -0400

    Finished Evening Scene Level
    Added all the enemies and obstacles for the evening scene. All enemies are animated, still need to fix the bee and add the respawn

---
+++ New Unity Project/Assets/enemie_movment_hunter.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemie_movment_hunter : MonoBehaviour
+{
+
+    public float speed;
+    public bool MoveRight;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (MoveRight) {
+            transform.Translate(14 * Time.deltaTime * speed, 0, 0);
+            transform.localScale = new Vector2(14, 14);
+
+        }
+        else {
+            
+            transform.Translate(-14 * Time.deltaTime * speed, 0, 0);
+            transform.localScale = new Vector2(-14, 14);
+        }
+
+    }
+
+    void OnTriggerEnter2D(Collider2D trig)
+    {
+
+        if (trig.gameObject.CompareTag("turn"))
+        {
+
+            if (MoveRight)
+            {
+                MoveRight = false;
+            }
+            else
+            {
+                MoveRight = true;
+            }
+        }
+
+
+    }
+}
